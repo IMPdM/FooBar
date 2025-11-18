ae7a921a4b5f46ae60f4d5b5a08d9acd569ee327
MatejVIM <matej.vnucec@im.si>
2025-11-18 17:10:16 +0100

    feat: izračun ter vizualni prikaz za slope ter dodatek EOLAttachmentBodyForTrain znotraj EOLAttachmentBody.py

--- db/queries/Sunstone/EOLAttachmentBody.py
+++ db/queries/Sunstone/EOLAttachmentBody.py
@@ -6,3 +6,11 @@ def EOLAttachmentBody(plant=196):
         FROM [WAREHOUSE].[dbo].[{plant}]
         ORDER BY [Timestamp] desc
     """
+
+def EOLAttachmentBodyForTrain(plant=196):
+    return f"""
+        SELECT [Timestamp], [18236] AS ScrawTCorr2PCorrJig2Average
+        FROM [WAREHOUSE].[dbo].[{plant}]
+        WHERE [Timestamp] >= ? AND [Timestamp] <= ?
+        ORDER BY [Timestamp]
+    """
